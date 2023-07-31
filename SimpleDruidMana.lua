90f5cc1cde860c727fbfd2be31c08490f1878949
anon <anon@qq.com>
2023-08-01 01:15:33 +0800

    Improved but still inaccurate

--- SimpleDruidMana.lua
+++ SimpleDruidMana.lua
@@ -8,12 +8,13 @@ local tmana = {
 	["max"] = 0,
 	["sip"] = 0,
 	["stance"] = 0,
+	["mp5"] = 0,
 }
-local costCached = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+local costCached = {0, 0, 0, 0, 0, 0, 0}
 local function GetShapeshiftCost(i)
 	if costCached[i] > 0 then return costCached[i] end
 	dummyTooltip:SetShapeshift(i)
-	local _, _, scost = string.find(dummyTooltip.costFontString:GetText() or "", "^(%d+)")
+	local _, _, scost = string.find(dummyTooltip.costFontString:GetText() or "", "(%d+)")
 	local cost = tonumber(scost or "0")
 	costCached[i] = cost
 	return cost
@@ -27,35 +28,51 @@ local function GetCurrentStance()
 	return 0
 end
 
-local function ManaBarUpdate(OnMana)
+-- TODO: Inaccurate mana
+local function ManaBarUpdate(onManaEvent)
 	local powerType = UnitPowerType(UNIT_PLAYER)
+	local base = floor(UnitStat(UNIT_PLAYER, 5) / 5) + 15
 	if powerType == POWERTYPE_MANA then
 		local cur = UnitMana(UNIT_PLAYER)
 		local max = UnitManaMax(UNIT_PLAYER)
-		local sip = cur - tmana.cur
-		if OnMana and sip > 0 and cur < max and tmana.cur > -1 then
-			if tmana.sip == 0 then DEFAULT_CHAT_FRAME:AddMessage("SimpleDruidMana is Ready") end
-			tmana.sip = sip
+		if onManaEvent and tmana.cur > -1 then
+			local sip = cur - tmana.cur
+			tmana.sip = sip > base and sip or base
+			-- DEFAULT_CHAT_FRAME:AddMessage("mana-form sip : " .. sip .. ", base : " .. base)
 		end
 		tmana.cur = cur
 		tmana.max = max
 		tmana.stance = 0
 		return
 	end
+	-- Bear/Cat form
 	if tmana.cur == -1 then return end
 	local stance = GetCurrentStance()
-	local cur = OnMana and min(tmana.cur + tmana.sip, tmana.max) or tmana.cur
 	if stance ~= tmana.stance then
 		tmana.stance = stance
-		cur = tmana.cur - GetShapeshiftCost(stance)
+		tmana.cur = tmana.cur - GetShapeshiftCost(stance)
+	elseif onManaEvent then
+		local sip = tmana.sip
+		if sip < base then
+			sip = base
+		elseif sip > base and tmana.mp5 > 0 then
+			sip = sip - base
+		end
+		tmana.cur = min(tmana.cur + sip, tmana.max)
+		-- DEFAULT_CHAT_FRAME:AddMessage("form:1/3 sip : " .. sip .. ", base : " .. base .. ", tmana.sip : " .. tmana.sip)
 	end
-	SimpleDruidManaBar:SetValue(cur / tmana.max)
-	SimpleDruidManaBarText:SetText(cur)
-	tmana.cur = cur
+	SimpleDruidManaBar:SetValue(tmana.cur / tmana.max)
+	SimpleDruidManaBarText:SetText(tmana.cur)
+end
+
+local function OnUpdate()
+	if tmana.mp5 > 0 then
+		tmana.mp5 = tmana.mp5 - arg1
+	end
 end
 
 function SimpleDruidMana_OnLoad(self)
-	local _, playerClass = UnitClass("player")
+	local _, playerClass = UnitClass(UNIT_PLAYER)
 	if playerClass ~= "DRUID" then
 		self:SetParent(nil)
 		return
@@ -66,31 +83,34 @@ function SimpleDruidMana_OnLoad(self)
 	self:RegisterEvent("PLAYER_LOGIN")
 	self:RegisterEvent("UNIT_MANA")
 	self:RegisterEvent("UNIT_DISPLAYPOWER")
+	self:RegisterEvent("SPELLCAST_STOP")
+
 	self:SetScript("OnEvent", function()
 		local event, arg1 = event, arg1
-		if arg1 == UNIT_PLAYER then
-			if event == "UNIT_MANA" then
-				ManaBarUpdate(true)
-			elseif event == "UNIT_DISPLAYPOWER"  then
-				local powerType = UnitPowerType(UNIT_PLAYER)
-				if powerType == POWERTYPE_MANA then
-					ManaBarUpdate(false)
-					self:Hide()
-				else
-					self:Show()
-				end
+		if event == "UNIT_MANA" and arg1 == UNIT_PLAYER then
+			ManaBarUpdate(true)
+		elseif event == "UNIT_DISPLAYPOWER" and arg1 == UNIT_PLAYER  then
+			local powerType = UnitPowerType(UNIT_PLAYER)
+			if powerType == POWERTYPE_MANA then
+				ManaBarUpdate(false)
+				self:Hide()
+			else
+				self:Show()
 			end
+		elseif event == "SPELLCAST_STOP" then -- SPELLCAST_INTERRUPTED, SPELLCAST_FAILURE
+			tmana.mp5 = 5.
 		elseif event == "PLAYER_LOGIN" then
-			-- for GameTooltip:SetShapeshift
+			-- Initialize
 			local frame = CreateFrame("GameTooltip")
 			frame:SetOwner(WorldFrame, "ANCHOR_NONE")
 			frame.costFontString = frame:CreateFontString()
 			frame:AddFontStrings(frame:CreateFontString(), frame:CreateFontString())
 			frame:AddFontStrings(frame.costFontString, frame:CreateFontString())
 			dummyTooltip = frame
-			-- Initialize
+			-- Sync
 			SimpleDruidManaBarText:SetFont(PlayerFrameManaBarText:GetFont())
 			ManaBarUpdate(false)
 		end
 	end)
+	self:SetScript("OnUpdate", OnUpdate)
 end
